cdf21d174e81bca31ec4355e25814f592767230d
Desour <ds.desour@proton.me>
2024-10-21 19:21:45 +0200

    collect abs node positions

--- src/client/content_mapblock.cpp
+++ src/client/content_mapblock.cpp
@@ -138,7 +138,8 @@ void MapblockMeshGenerator::drawQuad(v3f *coords, const v3s16 &normal,
 			applyFacesShading(vertices[j].Color, normal2);
 		vertices[j].TCoords = tcoords[j];
 	}
-	collector->append(cur_node.tile, vertices, 4, quad_indices, 6, cur_node.p);
+	collector->append(cur_node.tile, vertices, 4, quad_indices, 6,
+			blockpos_nodes + cur_node.p);
 }
 
 static std::array<video::S3DVertex, 24> setupCuboidVertices(const aabb3f &box, const f32 *txc, TileSpec *tiles, int tilecount) {
@@ -235,7 +236,8 @@ void MapblockMeshGenerator::drawCuboid(const aabb3f &box,
 		QuadDiagonal diagonal = face_lighter(k, &vertices[4 * k]);
 		const u16 *indices = diagonal == QuadDiagonal::Diag13 ? quad_indices_13 : quad_indices_02;
 		int tileindex = MYMIN(k, tilecount - 1);
-		collector->append(tiles[tileindex], &vertices[4 * k], 4, indices, 6, cur_node.p);
+		collector->append(tiles[tileindex], &vertices[4 * k], 4, indices, 6,
+				blockpos_nodes + cur_node.p);
 	}
 }
 
@@ -705,7 +707,8 @@ void MapblockMeshGenerator::drawLiquidSides()
 			pos += cur_node.origin;
 			vertices[j] = video::S3DVertex(pos.X, pos.Y, pos.Z, face.dir.X, face.dir.Y, face.dir.Z, cur_node.color, vertex.u, v);
 		};
-		collector->append(cur_liquid.tile, vertices, 4, quad_indices, 6, cur_node.p);
+		collector->append(cur_liquid.tile, vertices, 4, quad_indices, 6,
+				blockpos_nodes + cur_node.p);
 	}
 }
 
@@ -786,7 +789,8 @@ void MapblockMeshGenerator::drawLiquidTop()
 
 	std::swap(vertices[0].TCoords, vertices[2].TCoords);
 
-	collector->append(cur_liquid.tile_top, vertices, 4, quad_indices, 6, cur_node.p);
+	collector->append(cur_liquid.tile_top, vertices, 4, quad_indices, 6,
+			blockpos_nodes + cur_node.p);
 }
 
 void MapblockMeshGenerator::drawLiquidBottom()
@@ -804,7 +808,8 @@ void MapblockMeshGenerator::drawLiquidBottom()
 		vertices[i].Pos += cur_node.origin;
 	}
 
-	collector->append(cur_liquid.tile_top, vertices, 4, quad_indices, 6, cur_node.p);
+	collector->append(cur_liquid.tile_top, vertices, 4, quad_indices, 6,
+			blockpos_nodes + cur_node.p);
 }
 
 void MapblockMeshGenerator::drawLiquidNode()
@@ -1701,12 +1706,12 @@ void MapblockMeshGenerator::drawMeshNode()
 				vertex.Pos += cur_node.origin;
 			}
 			collector->append(cur_node.tile, vertices, vertex_count,
-				buf->getIndices(), buf->getIndexCount(), cur_node.p);
+				buf->getIndices(), buf->getIndexCount(), blockpos_nodes + cur_node.p);
 		} else {
 			// Don't modify the mesh, it may not be private here.
 			// Instead, let the collector process colors, etc.
 			collector->append(cur_node.tile, vertices, vertex_count,
-				buf->getIndices(), buf->getIndexCount(), cur_node.p,
+				buf->getIndices(), buf->getIndexCount(), blockpos_nodes + cur_node.p,
 				cur_node.origin, cur_node.color, cur_node.f->light_source);
 		}
 	}
